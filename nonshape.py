637a6fa84a3d00b0f5089ab3c366b184c4f00678
srcole <scott.cole0@gmail.com>
2016-11-27 20:34:06 -0800

    add fasthilbert option to amplitude time series calculation

--- nonshape.py
+++ nonshape.py
@@ -31,6 +31,7 @@ from __future__ import division
 import numpy as np
 from scipy import signal
 import scipy as sp
+import math
     
     
 def bandpass_default(x, f_range, Fs, rmv_edge = True, w = 3, plot_frequency_response = False):
@@ -139,7 +140,7 @@ def phaseT(x, frange, Fs, rmv_edge = False, filter_fn=None, filter_kwargs=None):
     return pha
     
     
-def ampT(x, frange, Fs, rmv_edge = False, filter_fn=None, filter_kwargs=None):
+def ampT(x, frange, Fs, rmv_edge = False, filter_fn=None, filter_kwargs=None, run_fasthilbert=False):
     """
     Calculate the amplitude time series
 
@@ -171,9 +172,30 @@ def ampT(x, frange, Fs, rmv_edge = False, filter_fn=None, filter_kwargs=None):
 
     # Filter signal
     xn, taps = filter_fn(x, frange, Fs, rmv_edge=rmv_edge, **filter_kwargs)
-    amp = np.abs(sp.signal.hilbert(xn))
+    if run_fasthilbert:
+        amp = np.abs(fasthilbert(xn))
+    else:
+        amp = np.abs(sp.signal.hilbert(xn))
 
     return amp
+    
+
+def fasthilbert(x, axis=-1):
+    """
+    Redefinition of scipy.signal.hilbert, which is very slow for some lengths
+    of the signal x. This version zero-pads the signal to the next power of 2
+    for speed.
+    """
+    x = np.array(x)
+    N = x.shape[axis]
+    N2 = 2**(int(math.log(len(x), 2)) + 1)
+    Xf = np.fft.fft(x, N2, axis=axis)
+    h = np.zeros(N2)
+    h[0] = 1
+    h[1:(N2 + 1) // 2] = 2
+
+    x = np.fft.ifft(Xf * h, axis=axis)
+    return x[:N]
 
 
 def findpt(x, f_range, Fs, boundary = None, forcestart = 'peak',
